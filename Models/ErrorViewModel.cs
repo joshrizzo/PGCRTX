4733c381331702a4e687595e46d1a3b26a031d11
jrizzo <josh.rizzo@gmail.com>
2018-02-02 12:13:56 -0600

    dotnet new mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PGCRTX.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
